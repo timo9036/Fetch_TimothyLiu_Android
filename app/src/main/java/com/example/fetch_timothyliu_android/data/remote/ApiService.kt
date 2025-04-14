0ae9305e517c2a4832bb01cee5e760e735d79d91
timo9036 <timo9036@hotmail.com>
2025-04-13 21:17:18 -0500

    retrofit

--- app/src/main/java/com/example/fetch_timothyliu_android/data/remote/ApiService.kt
+++ app/src/main/java/com/example/fetch_timothyliu_android/data/remote/ApiService.kt
@@ -1,9 +1,41 @@
 package com.example.fetch_timothyliu_android.data.remote
 
+import com.example.fetch_timothyliu_android.BuildConfig
 import com.example.fetch_timothyliu_android.data.model.NetworkItem
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 
 interface ApiService {
     @GET("hiring.json")
     suspend fun getItems(): List<NetworkItem>
+}
+
+object RetrofitInstance {
+    private const val BASE_URL = BuildConfig.FETCH_URL
+
+    private val moshi = Moshi.Builder()
+        .add(KotlinJsonAdapterFactory())
+        .build()
+
+    private val loggingInterceptor = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+
+    private val httpClient = OkHttpClient.Builder()
+        .addInterceptor(loggingInterceptor)
+        .build()
+
+    val api: ApiService by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(httpClient)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+            .create(ApiService::class.java)
+    }
 }
